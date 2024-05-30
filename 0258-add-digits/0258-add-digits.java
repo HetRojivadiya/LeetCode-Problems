efd184f2150f7e88f7b69c9ce5a706202a8deee6
Het_Rojivadiya <123486074+HetRojivadiya@users.noreply.github.com>
2024-05-30 20:13:36 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (78.29%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,29 +1,8 @@
 class Solution {
     public int addDigits(int num) {
-       
-        String n = String.valueOf(num);
-        
-        
-        while(true){
-            
-            int sum = 0;
-        
-            for(int i=0;i<n.length();i++)
-            {   
-                sum += Character.getNumericValue(n.charAt(i));
-            }
-            
-            if(String.valueOf(sum).length()==1)
-            {
-                return sum;
-               
-            }else{
-                n=String.valueOf(sum);
-               
-            }
-            
+        if (num == 0) {
+            return 0;
         }
-        
-        
+        return 1 + (num - 1) % 9;
     }
 }
