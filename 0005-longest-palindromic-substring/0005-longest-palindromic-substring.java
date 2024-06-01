9a70ee882f97bf5942175506ce8842397daa1ddd
Het_Rojivadiya <123486074+HetRojivadiya@users.noreply.github.com>
2024-06-01 17:53:54 +0530

    Time: 109 ms (46.14%), Space: 46.2 MB (18.19%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -2,24 +2,30 @@ class Solution {
     public String longestPalindrome(String s) {
         if (s == null || s.length() < 1) return "";
         
-        int start = 0, end = 0;
-        for (int i = 0; i < s.length(); i++) {
-            int len1 = expandAroundCenter(s, i, i);
-            int len2 = expandAroundCenter(s, i, i + 1);
-            int len = Math.max(len1, len2);
-            if (len > end - start) {
-                start = i - (len - 1) / 2;
-                end = i + len / 2;
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        
+        int start = 0;
+        int end = 0;
+        int maxLength = 0;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                // Palindromes of length 1 and 2
+                // For length 1, it's always a palindrome
+                // For length 2, both characters must be equal
+                // For length 2 or more, check if the characters at the ends are equal and if the substring between them is a palindrome
+                if (s.charAt(i) == s.charAt(j) && (i - j <= 2 || dp[j + 1][i - 1])) {
+                    dp[j][i] = true;
+                    if (i - j + 1 > maxLength) {
+                        maxLength = i - j + 1;
+                        start = j;
+                        end = i;
+                    }
+                }
             }
         }
+        
         return s.substring(start, end + 1);
-    }
-
-    private int expandAroundCenter(String s, int left, int right) {
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            left--;
-            right++;
-        }
-        return right - left - 1;
     }
 }
