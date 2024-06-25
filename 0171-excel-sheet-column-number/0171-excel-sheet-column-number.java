6d58aa3025a7851329e8181e51929582b9956bba
Het_Rojivadiya <123486074+HetRojivadiya@users.noreply.github.com>
2024-06-25 11:27:24 +0530

    Time: 4 ms (6.11%), Space: 42.1 MB (51.53%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
@@ -1,19 +1,13 @@
 class Solution {
     public int titleToNumber(String columnTitle) {
         
-        Map<Character,Integer> m = new HashMap<>();
-        char c = 'A'; 
-            
-        for(int i=1;i<27;i++)
-        {
-            m.put(c,i);
-            c++;
-        }
+       
+        System.out.print((int)'A');
         
         int sum = 0;
         for(int i=0;i<columnTitle.length();i++)
         {
-            sum+=m.get(columnTitle.charAt(i));
+            sum+=(int)columnTitle.charAt(i)-64;
             if(i+1<columnTitle.length())
             {
                 sum=sum*26;
