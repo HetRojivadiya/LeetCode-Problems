c404276018a9808789dd5887696c9cc5a148ed1d
Het_Rojivadiya <123486074+HetRojivadiya@users.noreply.github.com>
2024-07-16 00:23:57 +0530

    Time: 16 ms (6.58%), Space: 42.8 MB (12.34%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.java
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -1,36 +1,73 @@
 class Solution {
     public String makeGood(String s) {
         
-        while(true)
-        {
-            String temp="";
-            int flag=0;
-            for(int i=0;i<s.length();i++)
-            {
-                if(i<s.length()-1)
-                {
-                    if(Character.toLowerCase(s.charAt(i))==Character.toLowerCase(s.charAt(i+1)))
-                    {
-                        if((Character.isUpperCase(s.charAt(i)) == Character.isLowerCase(s.charAt(i+1)) ||
-                        Character.isUpperCase(s.charAt(i+1)) == Character.isLowerCase(s.charAt(i))))
-                        {       
-                            flag=1;
-                            i=i+1;
-                            continue;
-                        }   
-                    }
-                }
-                temp+=String.valueOf(s.charAt(i));
-            }
+//         while(true)
+//         {
+//             String temp="";
+//             int flag=0;
+//             for(int i=0;i<s.length();i++)
+//             {
+//                 if(i<s.length()-1)
+//                 {
+//                     if(Character.toLowerCase(s.charAt(i))==Character.toLowerCase(s.charAt(i+1)))
+//                     {
+//                         if((Character.isUpperCase(s.charAt(i)) == Character.isLowerCase(s.charAt(i+1)) ||
+//                         Character.isUpperCase(s.charAt(i+1)) == Character.isLowerCase(s.charAt(i))))
+//                         {       
+//                             flag=1;
+//                             i=i+1;
+//                             continue;
+//                         }   
+//                     }
+//                 }
+//                 temp+=String.valueOf(s.charAt(i));
+//             }
             
-            System.out.println(temp);
-            if(flag==1)
+//             System.out.println(temp);
+//             if(flag==1)
+//             {
+//                 s=temp;
+//             }else{
+//                 break;
+//             }
+//         }
+//         return s;
+        
+        Stack<Character> stack = new Stack();
+        
+        stack.push(s.charAt(0));
+        int i=1;
+        while(i!=s.length())
+        {
+            if(!stack.isEmpty())
             {
-                s=temp;
+                if(Character.toLowerCase(stack.peek())==Character.toLowerCase(s.charAt(i)) && (Character.isUpperCase(stack.peek()) == Character.isLowerCase(s.charAt(i)) || Character.isUpperCase(s.charAt(i)) == Character.isLowerCase(stack.peek())))
+            {
+                    System.out.print("h");
+                stack.pop();
             }else{
-                break;
+                stack.push(s.charAt(i));
+                
             }
+                
+            }
+            else{
+                    stack.push(s.charAt(i));
+                }
+            
+            i++;
         }
-        return s;
+                   
+                   
+        StringBuilder str = new StringBuilder();
+        
+                   while(!stack.isEmpty())
+                   {
+                       str.append(String.valueOf(stack.pop()));
+                   }
+                   
+                   return str.reverse().toString();
+        
+        
     }
 }
