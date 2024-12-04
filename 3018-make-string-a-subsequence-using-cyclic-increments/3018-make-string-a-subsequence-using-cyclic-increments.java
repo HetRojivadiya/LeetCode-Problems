0140911ff696073048a900ca7c22c736368727c4
Het_Rojivadiya <123486074+HetRojivadiya@users.noreply.github.com>
2024-12-04 18:26:42 +0530

    Time: 8 ms (9.02%), Space: 45.7 MB (36.84%) - LeetHub

---
+++ 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean canMakeSubsequence(String str1, String str2) {
+
+        if(str1.length()<str2.length())
+        {
+            return false;
+        }
+
+        int index=0;
+
+        for(int i=0;i<str1.length();i++)
+        {
+            if(str1.charAt(i)==str2.charAt(index) || str1.charAt(i)=='z' && str2.charAt(index)=='a' || (int)str1.charAt(i)+1==str2.charAt(index))
+            {
+                index++;
+            }
+
+            if(index==str2.length())
+            {
+                return true;
+            }
+        }
+
+        return false;
+        
+    }
+}
